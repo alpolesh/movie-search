ffa6049143d3e31b9f5d68ea73af87de58e3c0cc
Andrey Poleshchuck <alpolesh@gmail.com>
2020-04-28 23:15:36 +0300

    feat: add HTML Layout

--- movie-search/src/js/index.js
+++ movie-search/src/js/index.js
@@ -3,6 +3,19 @@ import '../css/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 
 
